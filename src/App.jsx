06e1901255c484fed5cd6095cb9b6cc24f9a46e5
Julio Arana Jr. <113112001+julioaranajr@users.noreply.github.com>
2024-05-22 13:45:28 +0000

    update about me

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Julio Arana Jr.",
+  title: "Cloud Systems Developer",
+  email: "contact@julioaranajr.com",
+  gitHub: "julioaranajr",
+  instagram: "julioaranajr",
+  linkedIn: "julioarana",
+  medium: "julio-arana-jr",
+  twitter: "julioaranajr",
+  youTube: "@julioaranajr",
 };
 
 const primaryColor = "#4E567E";
