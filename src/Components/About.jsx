35c443899f15525a8c0af00ff32e28c5c6674337
Julio Arana Jr. <113112001+julioaranajr@users.noreply.github.com>
2024-05-22 14:27:52 +0000

    update about me

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,28 +16,32 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/apple-retro-2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "retro 80 wallpaper with apple logo in the center";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I’m currently a Student @ DCI - Digital Career Institute";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+  "AWS",
+  "GCloud",
+  "Azure",
+  "Github",
+  "Markdown",
+  "Terraform",
+  "Python",
+  "JavaScript",
+  "MySQL",
+  "PostgreSQL",
+]
 
 /**
  * Use this to give more information about what you are passionate about,
