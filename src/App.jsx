cdee6611165f4c07923c1ede05b2939bc7d54317
Julio Arana Jr. <113112001+julioaranajr@users.noreply.github.com>
2024-05-22 15:04:24 +0000

    update name on app

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Julio Arana Jr.",
+  name: "Julio Arana",
   title: "Cloud Systems Developer",
   email: "contact@julioaranajr.com",
   gitHub: "julioaranajr",
